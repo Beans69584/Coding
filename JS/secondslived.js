56936cbccfedb561c1791455fbf726adbe31525f
Beans <101147193+Beans69584@users.noreply.github.com>
2022-12-13 14:49:49 +0000

    Updates

--- JS/secondslived.js
+++ JS/secondslived.js
@@ -2,7 +2,7 @@ function secondsLived(age) {
   return age * 365 * 24 * 60 * 60;
 }
 function Main() {
-  var age = 14;
+  var age = 77;
   var seconds = secondsLived(age);
   console.log(seconds);
 }
